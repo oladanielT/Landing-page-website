a97b3faf9e0966d10e4ef70cf28d934aae1e0c8d
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-08-07 10:47:48 +0100

    Added the Hero component

--- my_project_app/src/App.js
+++ my_project_app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Analytics from './components/Analytics';
+import Hero from './components/hero';
 import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
+        <Hero/>
         <Analytics/>
     </div>
   );
