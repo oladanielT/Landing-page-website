33793024ffed6a687199efcc5d62a00e10558000
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2023-08-07 10:28:02 +0100

    Added Analytics component

--- my_project_app/src/App.js
+++ my_project_app/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Analytics from './components/Analytics';
 
 function App() {
   return (
     <div className="App">
-        hello
+        <Analytics/>
     </div>
   );
 }
